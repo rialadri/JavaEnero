a158c3fb54dcd457689b8735c27274e1c5a445c6
rialadri <150134022+rialadri@users.noreply.github.com>
2024-01-09 12:13:03 +0100

    Mi tercer Commit

--- AdriJava/src/tutGit/EjemploGit.java
+++ AdriJava/src/tutGit/EjemploGit.java
@@ -5,6 +5,12 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// aqui va el codigo
+		
+		System.out.println("Hola Mundo, que buen dia hace");
+		
+		String nombre= "Adrian Madrigal";
+		System.out.println("Hola" + nombre);
+		
 	}
 
 }
